9da864e32a5d72315a8d9c85e9b3c7f6829d451c
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-23 14:56:19 -0300

    Entrada de dados

---
+++ sintaxe/EntradaDeDados.py
@@ -0,0 +1,19 @@
+nome1 = input("Nome da primeira pessoa: ")
+salario1 = float(input("Salario da primeira pessoa: "))
+idade1 = int(input("Digite a idade da primeira pessoa: "))
+sexo1 = input("Digite um sexo (F/M): ")
+
+nome2 = input("Nome da segunda pessoa: ")
+salario2 = float(input("Salario da segunda pessoa: "))
+idade2 = int(input("Digite a idade da segunda pessoa: "))
+sexo2 = input("Digite um sexo (F/M): ")
+
+print(f"Nome 1: {nome1}")
+print(f"Salario 1: {salario1:.2f}")
+print(f"Idade: {idade1}")
+print(f"Sexo: {sexo1}")
+
+print(f"Nome 2: {nome2}")
+print(f"Salario 2: {salario2:.2f}")
+print(f"Idade: {idade2}")
+print(f"Sexo: {sexo2}")
