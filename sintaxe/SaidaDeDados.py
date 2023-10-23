68bfb91abb62c9d8a5e2ae021a92cd35f52a198a
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-23 14:48:20 -0300

    Saida de dados

---
+++ sintaxe/SaidaDeDados.py
@@ -0,0 +1,20 @@
+print("Bom dia!", end="")
+print("Boa Noite!")
+
+x = "Maria"
+y = 19
+print("%s tem %d anos" %(x, y))
+
+x1 = 10
+y1 = 20
+print(x1)
+print(y1)
+
+x2 = 2.3456
+print("{:.2f}".format(x2))
+
+idade = 32
+salario = 4560.9
+nome = "Maria Silva"
+sexo = "F"
+print(f"A funcionaria {nome}, sexo {sexo}, ganha {salario:.2f} e tem {idade} anos")
