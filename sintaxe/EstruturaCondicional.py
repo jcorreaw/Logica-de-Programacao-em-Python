0008a2ff91f273f8b3cb632cfd373cdefe86f541
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-24 09:27:13 -0300

    Estrutura condicional

---
+++ sintaxe/EstruturaCondicional.py
@@ -0,0 +1,16 @@
+x = 10
+
+print("Bom dia")
+
+if x < 0:
+    print("Boa tarde")
+
+print("Boa noite")
+
+hora = int(input("Digite uma hora do dia: "))
+if hora < 12:
+    print("Bom dia!")
+elif hora < 18:
+    print("Boa tarde!")
+else:
+    print("Boa noite!")
